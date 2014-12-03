4cab1912f33f827912a1e6c5c7a062ffeb8c2633
Conan Yuan <conanyuan@gmail.com>
2014-12-03 20:25:36 +0000

    finished lab6

--- hw13/lab6.hs
+++ hw13/lab6.hs
@@ -104,9 +104,37 @@ ex7 = (*) (leaves . head . children . head . children $ xs) (product . map size 
 -- ===================================
 
 instance Functor Rose where
-  fmap = error "you have to implement fmap for Rose"
+  -- fmap = error "you have to implement fmap for Rose"
+  fmap f (r :> c) = f r :> (map (fmap f) c)
 
 ex10 = round . root . head . children . fmap (\x -> if x > 0.5 then x else 0) $ fmap (\x -> sin(fromIntegral x)) xs
+
+{-
+*Main> fmap (*2) (1 :> [2 :> [], 3 :> []])
+2 :> [4 :> [],6 :> []]
+*Main> fmap (+1) (1 :> [])
+2 :> []
+*Main> let tree = 1 :> map (\c -> c :> []) [1..5]
+*Main> size (fmap leaves (fmap (:> []) tree))
+6
+-}
+
+-- Which type signature?
+--
+-- Yes!
+-- f :: Rose a -> Rose a
+--
+-- The rest of these are bad:
+-- f :: Rose a -> Rose [a]
+-- f :: Rose a -> [Rose a]
+-- f :: Functor f => f a -> f b
+--
+-- f r = fmap head $ fmap (\x -> [x]) r
+
+{-
+*Main> round . root . head . children . fmap (\x -> if x > 0.5 then x else 0) $ fmap (\x -> sin(fromIntegral x)) xs
+1
+-}
 
 -- ===================================
 -- Ex. 11-13
@@ -120,23 +148,57 @@ newtype Sum a = Sum a
 newtype Product a = Product a
 
 instance Num a => Monoid (Sum a) where
-  mempty = error "you have to implement mempty for Sum"
-  mappend = error "you have to implement mappend for Sum"
-  
+  --mempty = error "you have to implement mempty for Sum"
+  mempty = Sum 0
+  --mappend = error "you have to implement mappend for Sum"
+  mappend (Sum a) (Sum b) = Sum (a + b)
+
 instance Num a => Monoid (Product a) where
-  mempty = error "you have to implement mempty for Product"
-  mappend = error "you have to implement mappend for Product"
+  --mempty = error "you have to implement mempty for Product"
+  --mappend = error "you have to implement mappend for Product"
+  mempty = Product 1
+  mappend (Product a) (Product b) = Product (a * b)
 
 unSum :: Sum a -> a
-unSum = error "you have to implement unSum"
+--unSum = error "you have to implement unSum"
+unSum (Sum a) = a
 unProduct :: Product a -> a
-unProduct = error "you have to implement unProduct"
+--unProduct = error "you have to implement unProduct"
+unProduct (Product a) = a
 
 num1 = mappend (mappend (Sum 2) (mappend (mappend mempty (Sum 1)) mempty)) (mappend (Sum 2) (Sum 1))
   
 num2 = mappend (Sum 3) (mappend mempty (mappend (mappend (mappend (Sum 2) mempty) (Sum (-1))) (Sum 3)))
   
 ex13 = unSum (mappend (Sum 5) (Sum (unProduct (mappend (Product (unSum num2)) (mappend (Product (unSum num1)) (mappend mempty (mappend (Product 2) (Product 3))))))))
+
+{-
+*Main> unProduct (Product 6 `mappend` (Product . unSum $ Sum 3 `mappend` Sum 4))
+42
+*Main> unSum num1
+6
+*Main> unSum num2
+7
+*Main> unSum $ Sum 3 `mappend` Sum 4
+7
+*Main> let num1 = mappend (mappend (Sum 2) (mappend (mappend mempty (Sum 1)) mempty)) (mappend (Sum 2) (Sum 1))
+*Main> let num2 = mappend (Sum 3) (mappend mempty (mappend (mappend (mappend (Sum 2) mempty) (Sum (-1))) (Sum 3)))
+*Main> unSum (mappend (Sum 5) (Sum (unProduct (mappend (Product (unSum num2)) (mappend (Product (unSum num1)) (mappend mempty (mappend (Product 2) (Product 3))))))))
+257
+-}
+
+-- Which is a valid type?
+--
+-- x :: Num num => Sum (unSum num)
+-- No
+--
+-- x :: Int int => Sum int
+-- No
+--
+-- x :: Num string => Sum string
+-- Yes!
+--
+-- x = Sum 3 `mappend` Sum 4
 
 -- ===================================
 -- Ex. 14-15
@@ -145,14 +207,26 @@ ex13 = unSum (mappend (Sum 5) (Sum (unProduct (mappend (Product (unSum num2)) (m
 class Functor f => Foldable f where
   fold :: Monoid m => f m -> m
   foldMap :: Monoid m => (a -> m) -> (f a -> m)
-  foldMap = error "you have to implement foldMap"
-  
+  -- foldMap = error "you have to implement foldMap"
+  foldMap a2m = \x -> fold (fmap a2m x)
+
 instance Foldable Rose where
-  fold = error "you have to implement fold for Rose"
-  
+  -- fold = error "you have to implement fold for Rose"
+  fold (r :> c) = foldr (\ ro i -> mappend i (fold ro)) r c
+
 sumxs = Sum 0 :> [Sum 13 :> [Sum 26 :> [Sum (-31) :> [Sum (-45) :> [], Sum 23 :> []]]], Sum 27 :> [], Sum 9 :> [Sum 15 :> [Sum 3 :> [Sum (-113) :> []], Sum 1 :> []], Sum 71 :> [Sum 55 :> []]]]
 
 ex15 = unSum (mappend (mappend (fold sumxs) (mappend (fold . head . drop 2 . children $ sumxs) (Sum 30))) (fold . head . children $ sumxs))
+
+{-
+*Main> let tree = 1 :> [2 :> [], 3 :> [4 :> []]]
+*Main> let tree' = fmap Product tree
+*Main> unProduct $ fold tree'
+24
+*Main> let sumxs = Sum 0 :> [Sum 13 :> [Sum 26 :> [Sum (-31) :> [Sum (-45) :> [], Sum 23 :> []]]], Sum 27 :> [], Sum 9 :> [Sum 15 :> [Sum 3 :> [Sum (-113) :> []], Sum 1 :> []], Sum 71 :> [Sum 55 :> []]]]
+*Main> unSum (mappend (mappend (fold sumxs) (mappend (fold . head . drop 2 . children $ sumxs) (Sum 30))) (fold . head . children $ sumxs))
+111
+-}
 
 -- ===================================
 -- Ex. 16-18
@@ -162,13 +236,37 @@ ex17 = unSum (mappend (mappend (foldMap (\x -> Sum x) xs) (mappend (foldMap (\x 
 
 ex18 = unSum (mappend (mappend (foldMap (\x -> Sum x) xs) (Sum (unProduct (mappend (foldMap (\x -> Product x) . head . drop 2 . children $ xs) (Product 3))))) (foldMap (\x -> Sum x) . head . children $ xs))
 
+{-
+*Main> let tree = 42 :> [3 :> [2:> [], 1 :> [0 :> []]]]
+*Main> unSum $ foldMap Sum tree
+48
+*Main> xs
+0 :> [1 :> [2 :> [3 :> [4 :> [],5 :> []]]],6 :> [],7 :> [8 :> [9 :> [10 :> []],11 :> []],12 :> [13 :> []]]]
+*Main> unSum (mappend (mappend (foldMap (\x -> Sum x) xs) (mappend (foldMap (\x -> Sum x) . head . drop 2 . children $ xs) (Sum 30))) (foldMap (\x -> Sum x) . head . children $ xs))
+206
+*Main> unSum (mappend (mappend (foldMap (\x -> Sum x) xs) (Sum (unProduct (mappend (foldMap (\x -> Product x) . head . drop 2 . children $ xs) (Product 3))))) (foldMap (\x -> Sum x) . head . children $ xs))
+25946026
+-}
+
 -- ===================================
 -- Ex. 19-21
 -- ===================================
 
 fproduct, fsum :: (Foldable f, Num a) => f a -> a
-fsum = error "you have to implement fsum"
-fproduct = error "you have to implement fproduct"
+-- fsum = error "you have to implement fsum"
+fsum a = unSum $ foldMap Sum a
+-- fproduct = error "you have to implement fproduct"
+fproduct a = unProduct $ foldMap Product a
 
 ex21 = ((fsum . head . drop 1 . children $ xs) + (fproduct . head . children . head . children . head . drop 2 . children $ xs)) - (fsum . head . children . head . children $ xs)
 
+{-
+*Main> fsum xs
+91
+*Main> fproduct xs
+0
+*Main> xs
+0 :> [1 :> [2 :> [3 :> [4 :> [],5 :> []]]],6 :> [],7 :> [8 :> [9 :> [10 :> []],11 :> []],12 :> [13 :> []]]]
+*Main> ((fsum . head . drop 1 . children $ xs) + (fproduct . head . children . head . children . head . drop 2 . children $ xs)) - (fsum . head . children . head . children $ xs)
+82
+-}
